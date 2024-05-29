9ca5f04d5416f4f1c386e84cb98b5e92b1138c03
kais-blkc <nagliyavarec@gmail.com>
2024-05-29 18:18:24 +0300

    Spaces test

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -3,23 +3,23 @@ import Image from 'next/image';
 import { FC } from 'react';
 
 export const Header: FC = () => {
-	return (
-		<header>
-			<div className="container">
-				<Grid container direction="row" justifyContent="space-between" alignItems="center" gap={3}>
-					<Button variant="outlined" color="inherit">
-						Покемоны API
-					</Button>
+  return (
+    <header>
+      <div className="container">
+        <Grid container direction="row" justifyContent="space-between" alignItems="center" gap={3}>
+          <Button variant="outlined" color="inherit">
+            Покемоны API
+          </Button>
 
-					<div className="flex-img-text">
-						<Image src="/hand.svg" alt="Нажмите на нужное Покемона" width={24} height={30} />
-						<p>
-							Нажмите на <br />
-							нужного Покемона
-						</p>
-					</div>
-				</Grid>
-			</div>
-		</header>
-	);
+          <div className="flex-img-text">
+            <Image src="/hand.svg" alt="Нажмите на нужное Покемона" width={24} height={30} />
+            <p>
+              Нажмите на <br />
+              нужного Покемона
+            </p>
+          </div>
+        </Grid>
+      </div>
+    </header>
+  );
 };
